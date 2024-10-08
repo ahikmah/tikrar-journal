267715c30d0b4ade67aaedde4925d3dd2883d536
Awaliyatul Hikmah <imadlz15@gmail.com>
2024-10-08 11:58:18 +0700

    Update JWT sign-in file paths to remove redundant "jwt" directory

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -20,8 +20,8 @@ export const paths = {
       resetPassword: `${ROOTS.AUTH}/amplify/reset-password`,
     },
     jwt: {
-      signIn: `${ROOTS.AUTH}/jwt/sign-in`,
-      signUp: `${ROOTS.AUTH}/jwt/sign-up`,
+      signIn: `${ROOTS.AUTH}/sign-in`,
+      signUp: `${ROOTS.AUTH}/sign-up`,
     },
     firebase: {
       signIn: `${ROOTS.AUTH}/firebase/sign-in`,

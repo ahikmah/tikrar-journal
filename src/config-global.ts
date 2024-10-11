a0b0c68d4c5433aef14245bc35ff32ed9d8a0805
Awaliyatul Hikmah <imadlz15@gmail.com>
2024-10-11 11:27:20 +0700

    chore: Add apiQuranUrl to global config

--- src/config-global.ts
+++ src/config-global.ts
@@ -8,6 +8,7 @@ export type ConfigValue = {
   appName: string;
   appVersion: string;
   serverUrl: string;
+  apiQuranUrl: string;
   assetsDir: string;
   isStaticExport: boolean;
   auth: {
@@ -15,7 +16,6 @@ export type ConfigValue = {
     skip: boolean;
     redirectPath: string;
   };
-  mapboxApiKey: string;
   firebase: {
     appId: string;
     apiKey: string;
@@ -25,9 +25,6 @@ export type ConfigValue = {
     measurementId: string;
     messagingSenderId: string;
   };
-  amplify: { userPoolId: string; userPoolWebClientId: string; region: string };
-  auth0: { clientId: string; domain: string; callbackUrl: string };
-  supabase: { url: string; key: string };
 };
 
 // ----------------------------------------------------------------------
@@ -36,6 +33,7 @@ export const CONFIG: ConfigValue = {
   appName: 'Tikrar Journal',
   appVersion: packageJson.version,
   serverUrl: process.env.NEXT_PUBLIC_SERVER_URL ?? '',
+  apiQuranUrl: process.env.NEXT_PUBLIC_QURAN_URL ?? '',
   assetsDir: process.env.NEXT_PUBLIC_ASSETS_DIR ?? '',
   isStaticExport: JSON.parse(`${process.env.BUILD_STATIC_EXPORT}`),
   /**
@@ -48,10 +46,6 @@ export const CONFIG: ConfigValue = {
     redirectPath: paths.main.root,
   },
   /**
-   * Mapbox
-   */
-  mapboxApiKey: process.env.NEXT_PUBLIC_MAPBOX_API_KEY ?? '',
-  /**
    * Firebase
    */
   firebase: {
@@ -62,28 +56,5 @@ export const CONFIG: ConfigValue = {
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? '',
     appId: process.env.NEXT_PUBLIC_FIREBASE_APPID ?? '',
     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ?? '',
-  },
-  /**
-   * Amplify
-   */
-  amplify: {
-    userPoolId: process.env.NEXT_PUBLIC_AWS_AMPLIFY_USER_POOL_ID ?? '',
-    userPoolWebClientId: process.env.NEXT_PUBLIC_AWS_AMPLIFY_USER_POOL_WEB_CLIENT_ID ?? '',
-    region: process.env.NEXT_PUBLIC_AWS_AMPLIFY_REGION ?? '',
-  },
-  /**
-   * Auth0
-   */
-  auth0: {
-    clientId: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID ?? '',
-    domain: process.env.NEXT_PUBLIC_AUTH0_DOMAIN ?? '',
-    callbackUrl: process.env.NEXT_PUBLIC_AUTH0_CALLBACK_URL ?? '',
-  },
-  /**
-   * Supabase
-   */
-  supabase: {
-    url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-    key: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
   },
 };

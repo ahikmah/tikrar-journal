d3e30f84dabac477223694cfad8681eca87301bc
ohfufu <awalhikmah99@gmail.com>
2024-10-13 22:08:55 +0700

    feat: Add camelize and decamelize utility functions

--- src/utils/camelize.ts
+++ src/utils/camelize.ts
@@ -1,24 +1,19 @@
 type AnyObject = { [key: string]: any };
 type Options = {
-  separator?: string;
-  split?: RegExp;
   process?: (key: string, convert: (str: string) => string, options?: Options) => string;
 };
 
 const isObject = (obj: any): obj is AnyObject => obj === Object(obj);
 const isArray = (obj: any): obj is any[] => Array.isArray(obj);
 
-const separateWords = (str: string, options: Options = {}): string => {
-  const separator = options.separator || '_';
-  const split = options.split || /(?=[A-Z])/;
-  return str.split(split).join(separator);
-};
-
-function decamelize(str: string, options?: Options): string {
-  return separateWords(str, options).toLowerCase();
+function camelize(str: string): string {
+  if (str.match(/^\d+$/)) return str;
+  return str
+    .replace(/[-_](.)/g, (_, chr) => chr.toUpperCase())
+    .replace(/^(.)/, (chr) => chr.toLowerCase());
 }
 
-const processKeys = (convert: (str: string) => string, obj: any, options?: Options): any => {
+function processKeys(convert: (str: string) => string, obj: any, options?: Options): any {
   if (!isObject(obj) || isArray(obj)) return obj;
 
   const output: AnyObject = {};
@@ -28,15 +23,15 @@ const processKeys = (convert: (str: string) => string, obj: any, options?: Optio
     }
   });
   return output;
-};
+}
 
-const processor = (convert: (str: string) => string, options?: Options) => {
+function processor(convert: (str: string) => string, options?: Options) {
   const callback = options?.process;
   if (typeof callback !== 'function') return convert;
 
   return (str: string) => callback(str, convert, options);
-};
+}
 
-export function decamelizeKeys(object: AnyObject, options?: Options): AnyObject {
-  return processKeys(processor(decamelize, options), object, options);
+export function camelizeKeys(object: AnyObject, options?: Options): AnyObject {
+  return processKeys(processor(camelize, options), object, options);
 }

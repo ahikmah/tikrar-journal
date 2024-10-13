8dbf7ed4d537f63142e6cdd7a9a1997487e1e305
ohfufu <awalhikmah99@gmail.com>
2024-10-13 23:07:34 +0700

    feat: Update Quran utility functions and types

--- src/utils/quran.ts
+++ src/utils/quran.ts
@@ -1,3 +1,4 @@
+import type Reciter from 'src/types/reciter';
 import type { VersesResponse } from 'src/types/api-quran-response';
 import type { PagesLookUpRequest } from 'src/types/api-quran-request';
 
@@ -17,6 +18,8 @@ export const getPageVersesParams = (mushafId: Mushaf, wordFields: { wordFields: 
   filterPageWords: true,
   ...wordFields,
 });
+
+// ----------------------------------------------------------------------
 
 export const getPagesLookupParams = (
   resourceId: number | string,
@@ -62,6 +65,8 @@ export const getPagesLookupParams = (
   return params;
 };
 
+// ----------------------------------------------------------------------
+
 /**
  * Get the default word fields that should exist in the response.
  * qpc_uthmani_hafs is added so that we can use it as a fallback
@@ -78,6 +83,8 @@ export const getDefaultWordFields = (
     quranFont === QuranFont.TajweedV4 ? QuranFont.MadaniV2 : quranFont
   }${quranFont === QuranFont.QPCHafs ? '' : `,${QuranFont.QPCHafs}`}`,
 });
+
+// ----------------------------------------------------------------------
 
 /**
  * Get the mushaf id based on the value inside redux (if it's not SSR).
@@ -98,4 +105,70 @@ export const getMushafId = (
       mushafLines === MushafLines.FifteenLines ? Mushaf.Indopak15Lines : Mushaf.Indopak16Lines;
   }
   return { mushaf };
+};
+
+// ----------------------------------------------------------------------
+
+export const ITEMS_PER_PAGE = 10;
+
+export const DEFAULT_TRANSLATIONS = [131]; // Dr. Mustafa Khattab, the Clear Quran
+
+const DEFAULT_WBW_LOCALE = 'id';
+
+export const DEFAULT_VERSES_PARAMS = {
+  words: true,
+  translationFields: 'resource_name,language_id', // needed to show the name of the translation
+  perPage: ITEMS_PER_PAGE,
+  fields: `${QuranFont.Uthmani},chapter_id,hizb_number,text_imlaei_simple`, // we need text_uthmani field when copying the verse. text_imlaei_simple is for SEO description meta tag. Also the chapter_id for when we want to share the verse or navigate to Tafsir, hizb_number is for when we show the context menu.
+};
+
+export const DEFAULT_RECITER = {
+  id: 7,
+  name: 'Mishari Rashid al-`Afasy',
+  recitationStyle: 'Warsh',
+  relativePath: 'mishaari_raashid_al_3afaasee',
+} as Reciter;
+
+export const DEFAULT_QURAN_READER_STYLES = {
+  tafsirFontScale: 3,
+  quranTextFontScale: 3,
+  translationFontScale: 3,
+  wordByWordFontScale: 3,
+  quranFont: QuranFont.MadaniV1,
+  mushafLines: MushafLines.SixteenLines,
+  isUsingDefaultFont: true,
+};
+
+/**
+ * Use the default params and allow overriding the default values e.g. translations.
+ *
+ * @param {string} currentLocale
+ * @param {Record<string, unknown>} params
+ * @param {boolean} includeTranslationFields
+ * @returns {Record<string, unknown>}
+ */
+export const getVersesParams = (
+  currentLocale: string,
+  params?: Record<string, unknown>,
+  includeTranslationFields = true
+): Record<string, unknown> => {
+  const defaultParams = {
+    ...DEFAULT_VERSES_PARAMS,
+    translations: DEFAULT_TRANSLATIONS.join(', '),
+    reciter: DEFAULT_RECITER.id,
+    wordTranslationLanguage: DEFAULT_WBW_LOCALE,
+  };
+
+  if (defaultParams && !includeTranslationFields) {
+    Object.keys(defaultParams).forEach((key) => {
+      if (key === 'translationFields' || key === 'translations') {
+        delete defaultParams[key];
+      }
+    });
+  }
+
+  return {
+    ...defaultParams,
+    ...params,
+  };
 };

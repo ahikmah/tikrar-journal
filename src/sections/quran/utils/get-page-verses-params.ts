eb8b8c4d57df51c19ed8ff34cd62bf4254bb6cc6
ohfufu <awalhikmah99@gmail.com>
2024-10-13 23:08:13 +0700

    feat: Remove unused utility function

--- src/sections/quran/utils/get-page-verses-params.ts
+++
@@ -1,8 +0,0 @@
-import type { Mushaf } from 'src/types/quran-reader';
-
-export const getPageVersesParams = (mushafId: Mushaf, wordFields: { wordFields: string }) => ({
-  perPage: 'all',
-  mushaf: mushafId,
-  filterPageWords: true,
-  ...wordFields,
-});
